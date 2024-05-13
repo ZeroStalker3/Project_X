62f9fa78f3342b57b084e82dc88d040c3265b0c6
Zero <stalker_mr@list.ru>
2024-05-14 00:55:33 +0900

    Update 0.1

--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Project.Class;
+using Project.PageM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,9 @@ namespace Project
         public MainWindow()
         {
             InitializeComponent();
+
+            PageNext.frmObj = FrmMain;
+            FrmMain.Navigate(new PageAuth());
         }
     }
 }
