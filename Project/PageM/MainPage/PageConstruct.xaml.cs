1a6d874328a27274148165a7328e9874447ca2ef
Zero <stalker_mr@list.ru>
2024-05-16 17:55:54 +0900

    Update Constructor

--- Project/PageM/MainPage/PageConstruct.xaml.cs
+++ Project/PageM/MainPage/PageConstruct.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace Project.PageM.MainPage
 {
-    /// <summary>
-    /// Interaction logic for PageConstruct.xaml
-    /// </summary>
     public partial class PageConstruct : Page
     {
         public PageConstruct()
@@ -25,6 +22,90 @@ namespace Project.PageM.MainPage
             InitializeComponent();
         }
 
+        private void DropTargetRectangle_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+                DropTargetRectangle.Fill = Brushes.Green;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
 
+        private void DropTargetRectangle_DragLeave(object sender, DragEventArgs e)
+        {
+            DropTargetRectangle.Fill = Brushes.LightGray;
+        }
+
+        private void DropTargetRectangle_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void DropTargetRectangle_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files.Length > 0)
+                {
+                    string filePath = files[0];
+                    try
+                    {
+                        BitmapImage bitmap = new BitmapImage(new Uri(filePath));
+                        DroppedImage.Source = bitmap;
+
+                        // Установить CenterX и CenterY для поворота из центра изображения
+                        bitmap.DownloadCompleted += (s, ev) =>
+                        {
+                            ImageRotateTransform.CenterX = DroppedImage.ActualWidth / 2;
+                            ImageRotateTransform.CenterY = DroppedImage.ActualHeight / 2;
+                            ImageScaleTransform.CenterX = DroppedImage.ActualWidth / 2;
+                            ImageScaleTransform.CenterY = DroppedImage.ActualHeight / 2;
+                        };
+
+                        DropTargetRectangle.Fill = Brushes.LightGray;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error loading image: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void RotationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (ImageRotateTransform != null)
+            {
+                ImageRotateTransform.Angle = e.NewValue;
+            }
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (ImageScaleTransform != null)
+            {
+                ImageScaleTransform.ScaleX = e.NewValue;
+            }
+        }
+
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (ImageScaleTransform != null)
+            {
+                ImageScaleTransform.ScaleY = e.NewValue;
+            }
+        }
     }
 }
